713cd3aa3a85adc9219af6ce724f7c208d8cef2b
Adafede <adriano.rutz@unige.ch>
2022-02-23 13:34:51 +0100

    adding semi batch mode

--- inst/scripts/export_for_yaccl.R
+++ inst/scripts/export_for_yaccl.R
@@ -1,16 +1,23 @@
 library(dplyr)
 library(readr)
 
-classified_path <-
+lotus_metadata_path <-
   "~/Git/lotus-processor/data/processed/220208_frozen_metadata.csv.gz"
 
 forYaccl_path <- "~/Git/elotus-trees/data/smiles4yaccl.txt"
 
+fromYaccl_path <- "~/Downloads/tmp/lotus/yaccl"
+
+already_classified <- list.files(fromYaccl_path) |> 
+  gsub(pattern = ".txt",
+       replacement = "")
+
 message("exporting unique LOTUS 2D structures for yaccl classification")
 smiles_2D_classified <- readr::read_delim(
-  file = classified_path,
+  file = lotus_metadata_path,
   col_select = c("smiles" = "structure_smiles_2D")
 ) |>
-  dplyr::distinct() |>
+  dplyr::distinct() |> 
+  dplyr::filter(!smiles %in% already_classified) |>
   readr::write_tsv(file = forYaccl_path,
                    col_names = FALSE)
