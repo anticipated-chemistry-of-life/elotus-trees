cdf10a64ef148e229dd70d50aa742f640cb15179
Adafede <adriano.rutz@unige.ch>
2022-02-23 17:09:29 +0100

    adding first exploration script of yaccl results

---
+++ inst/scripts/treat_yaccl.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(jsonlite)
+library(readr)
+
+fromYaccl_path <- "~/Downloads/tmp/lotus/yaccl"
+
+already_classified <- list.files(fromYaccl_path, full.names = TRUE)
+
+classification_list <-
+  lapply(
+    X = already_classified,
+    FUN = function(x) {
+      jsonlite::fromJSON(x) |>
+        data.frame()
+    }
+  )
+
+full <- dplyr::bind_rows(classification_list,)
+
+classified <- full |> 
+  dplyr::filter(hits.classification_names != "NULL")
