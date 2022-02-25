fbf04a6417fb181b02c5c803e4f7c8e03f1d05fa
Adafede <adriano.rutz@unige.ch>
2022-02-25 19:29:26 +0100

    lint

--- R/make_2D.R
+++ R/make_2D.R
@@ -12,6 +12,6 @@ make_2D <-
     df |>
       dplyr::group_by(!!as.name(names(df)[!grepl(pattern = "structure", x = names(df))])) |>
       dplyr::distinct(structure_smiles_2D, .keep_all = TRUE) |>
-      dplyr::ungroup() |> 
+      dplyr::ungroup() |>
       dplyr::select(-structure_smiles_2D)
   }
