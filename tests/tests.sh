7cd34427886b727258cdeb40bfb49f6b51613dd6
Adafede <adriano.rutz@unige.ch>
2022-02-25 20:07:25 +0100

    add rough tests

---
+++ tests/tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ ! -d config ]; then
+  echo "Sorry, you need to run that from where your config is."
+  exit 1
+fi
+
+cp -R config/default config/params &&
+Rscript inst/scripts/count_npclasses.R &&
+Rscript inst/scripts/prepare_pseudoabsence_tables.R &&
+Rscript inst/scripts/prettyStructuresTable.R &&
+Rscript inst/scripts/prettyTree.R
