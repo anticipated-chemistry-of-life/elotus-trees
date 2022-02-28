6c0649c5732e3c74a30f9b910629b623c723e0f1
Adafede <adriano.rutz@unige.ch>
2022-02-28 10:22:36 +0100

    update

--- inst/scripts/treat_yaccl.R
+++ inst/scripts/treat_yaccl.R
@@ -1,16 +1,23 @@
 source(file = "R/log_debug.R")
 start <- Sys.time()
 
+#' TODO move in path
+path_yaccl_hierarchy <-
+  "https://raw.githubusercontent.com/rwst/yaccl/main/src/data-class-subclass.json"
+path_yaccl_hierarchy_names <-
+  "https://raw.githubusercontent.com/rwst/yaccl/main/src/data-class-subclass-names.json"
+
 #' Packages
 packages_cran <-
-  c("dplyr", "jsonlite", "jsonlite", "readr")
+  c("dplyr", "jsonlite", "jsonlite", "readr", "tidyr")
 packages_bioconductor <- NULL
 packages_github <- NULL
 
 source(file = "R/check_and_load_packages.R")
 check_and_load_packages()
 
-fromYaccl_path <- "~/Git/lotus-processor/data/interim/dictionaries_full/structure/yaccl"
+fromYaccl_path <-
+  "~/Git/lotus-processor/data/interim/dictionaries_full/structure/yaccl"
 
 already_classified <- list.files(fromYaccl_path, full.names = TRUE)
 
@@ -27,6 +34,103 @@ full <- dplyr::bind_rows(classification_list)
 
 classified <- full |>
   dplyr::filter(hits.classification_names == "NULL")
+
+yaccl_hierarchy <- jsonlite::fromJSON(txt = path_yaccl_hierarchy)
+
+yaccl_hierarchy_names <-
+  jsonlite::fromJSON(txt = path_yaccl_hierarchy_names)
+
+manual_corrections <-
+  list("item" = "Q134219", "super" = "Q2553138") |>
+  data.frame()
+
+yaccl_hierarchy <- yaccl_hierarchy |>
+  dplyr::anti_join(manual_corrections)
+
+yaccl_hierarchy_expanded <- yaccl_hierarchy |>
+  dplyr::distinct(item, parent_01 = super) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_02 = super),
+    by = c("parent_01" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_03 = super),
+    by = c("parent_02" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_04 = super),
+    by = c("parent_03" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_05 = super),
+    by = c("parent_04" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_06 = super),
+    by = c("parent_05" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_07 = super),
+    by = c("parent_06" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_08 = super),
+    by = c("parent_07" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_09 = super),
+    by = c("parent_08" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_10 = super),
+    by = c("parent_09" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_11 = super),
+    by = c("parent_10" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_12 = super),
+    by = c("parent_11" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_13 = super),
+    by = c("parent_12" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_14 = super),
+    by = c("parent_13" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_15 = super),
+    by = c("parent_14" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_16 = super),
+    by = c("parent_15" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_17 = super),
+    by = c("parent_16" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_18 = super),
+    by = c("parent_17" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_19 = super),
+    by = c("parent_18" = "item")
+  ) |>
+  dplyr::left_join(yaccl_hierarchy |> dplyr::distinct(item, parent_20 = super),
+    by = c("parent_19" = "item")
+  ) |>
+  dplyr::select_if(function(x) {
+    any(!is.na(x))
+  })
+
+yaccl_hierarchy_named <- yaccl_hierarchy_expanded |>
+  tidyr::pivot_longer(2:ncol(yaccl_hierarchy_expanded)) |>
+  dplyr::left_join(yaccl_hierarchy_names |> dplyr::distinct(item, item_label = lab)) |>
+  dplyr::left_join(
+    yaccl_hierarchy_names |> dplyr::distinct(item, parentLabel = lab),
+    by = c("value" = "item")
+  ) |>
+  dplyr::filter(!is.na(value)) |>
+  dplyr::mutate(level = gsub(
+    pattern = "parent_",
+    replacement = "",
+    x = name
+  )) |>
+  dplyr::select(
+    id = item,
+    label = item_label,
+    parentLevel = level,
+    parent = value,
+    parentLabel
+  ) |>
+  dplyr::distinct()
 
 end <- Sys.time()
 
